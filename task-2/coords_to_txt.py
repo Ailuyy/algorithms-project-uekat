cc64affbd97ca3d9817568d87d9d04322b10041d
Julka <julialtnc@gmail.com>
2023-11-19 12:23:07 +0100

    added 2 functions: main and for the points generation

--- task-2/coords_to_txt.py
+++ task-2/coords_to_txt.py
@@ -1,13 +1,11 @@
-def to_file(n_points, points, txt_file):
-    with open("txt_file.txt", "w") as f:
-        f.write(f"Number of points: {n_points}\n\nPoints:\n" + "\n".join(f"Point: x = {lat} y = {lon}" for lat, lon in points) + "\n\n")
+def to_file(points, txt_file):
+   with open("txt_file.txt", "w") as f:
+       f.write(f"\n".join(f"{lat} % {lon}" for lat, lon in points) + "\n")
 def from_file(txt_file):
-    points = []
-    with open(txt_file, "r") as f:
-        lines = f.readlines()
-        n_points = len(lines)
-        num_paths = int(lines[1].split(":")[-1].strip())
-        for i in range(2, 2 + n_points):
-            x, y = float(lines[i].split("x =")[-1].split()[0]), float(lines[i].split("y =")[-1])
-            points.append((x, y))
-    return n_points, points
+   points = []
+   with open(txt_file, "r") as f:
+       lines = f.readlines()
+       for i in range(0, len(lines)):
+           x, y = float(lines[i].split('%')[0].split()[0]), float(lines[i].split('%')[1])
+           points.append((x, y))
+   return points
--- task-2/finding_the_path.py
+++ task-2/finding_the_path.py
@@ -1,41 +1,33 @@
-import random as rnd, numpy as np, matplotlib.pyplot as plt, coords_to_txt as coords
+import random as rnd, numpy as np, coords_to_txt as coords
 rnd.seed(42)
-# TODO: implement options to select export or import of data
-def generate_data():
-    n_points, coords_limits = rnd.randint(10, 15), {"min":0.00, "max":50.00}
-    points = [(round(rnd.uniform(coords_limits["min"], coords_limits["max"]), 2), round(rnd.uniform(coords_limits["min"], coords_limits["max"]), 2)) for _ in range(n_points)]
-    start_point = rnd.choice(points)
-    end_point = rnd.choice([point for point in points if point != start_point])
-    k = rnd.randint(1, min(n_points - 2, len(points) - 2))
-    return n_points, points, start_point, end_point, k
+def generate_points(k):
+   n_points, coords_limits = rnd.randint(k, k + rnd.randint(10, 100)), {"min":0.00, "max":50.00}
+   points = [(round(rnd.uniform(coords_limits["min"], coords_limits["max"]), 2), round(rnd.uniform(coords_limits["min"], coords_limits["max"]), 2)) for _ in range(n_points)]
+   return points
 def euclidean_distance(p1, p2):
-    return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
-def find_closest_point(target, all_points):
-    return min(all_points, key=lambda point: euclidean_distance(target, point))
+   return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+def find_closest_point(target, points):
+   return min(points, key=lambda point: euclidean_distance(target, point))
 def greedy_algorithm(points, start, end, k):
-    k_points = [point for point in points if point not in [start, end]]
-    current_point = start
-    path = [start]
-    for _ in range(k):
-        closest_point = find_closest_point(current_point, k_points)
-        path.append(closest_point)
-        k_points.remove(closest_point)
-        current_point = closest_point
-    path.append(end)
-    total_distance = sum(euclidean_distance(path[i], path[i + 1]) for i in range(len(path) - 1))
-    return path, total_distance
-data = generate_data()
-path, total_distance = greedy_algorithm(data[1], data[2], data[3], data[4])
-x, y = zip(*data[1])
-plt.scatter(x, y, color='blue', label='Points', s=100)
-x, y = zip(*path)
-plt.plot(x, y, color='blue', marker='o', linestyle='-', linewidth=2, markersize=5, label='Path')
-plt.scatter(*data[2], color='orange', label='Start Point', s=100)
-plt.scatter(*data[3], color='red', label='End Point', s=100)
-plt.legend()
-plt.xlabel('X Coordinate')
-plt.ylabel('Y Coordinate')
-plt.title('Shortest Path')
-plt.show()
-coords.to_file(data[0], data[1], "txt_file.txt")
-print(f"Start point: {data[2]}\nEnd point: {data[3]}\nK points: {data[4]}\nShortest Path: {';'.join(f'x = {lat} y = {lon}' for lat, lon in path)}\nTotal Distance: {round(total_distance, 2)}")
+   k_points = [point for point in points if point not in [start, end]]
+   current_point = start
+   path = [start]
+   for _ in range(k):
+       closest_point = find_closest_point(current_point, k_points)
+       path.append(closest_point)
+       k_points.remove(closest_point)
+       current_point = closest_point
+   path.append(end)
+   total_distance = sum(euclidean_distance(path[i], path[i + 1]) for i in range(len(path) - 1))
+   return path, total_distance
+def main():
+   k = int(input("How many K points do you need?\n"))
+   decision = input("Do you want to import points from data.txt? Y/N\n")
+   points = coords.from_file("data.txt") if str.upper(decision) == "Y" else generate_points(k)
+   start_point = rnd.choice(points)
+   end_point = rnd.choice([point for point in points if point != start_point])
+   path, total_distance = greedy_algorithm(points, start_point, end_point, k)
+   coords.to_file(points, "txt_file.txt") if str.upper(decision) == "N" else ""
+   print(f"Start point: {start_point}\nEnd point: {end_point}\nN points: {len(points)}\nK points: {k}\nShortest Path: {';'.join(f'x = {lat} y = {lon}' for lat, lon in path)}\nTotal Distance: {round(total_distance, 2)}")
+if __name__ == "__main__":
+   main()
