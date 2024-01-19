e4110a95bbd34564009d7f6c91f7db05a651826c
Julka <julialtnc@gmail.com>
2024-01-19 11:47:00 +0100

    restructuring the code

--- task-3/filling_the_space.py
+++ task-3/filling_the_space.py
@@ -1,5 +1,35 @@
-from cat_class import Cat
-from hunting_item_class import HuntingItem
+import random as rnd
+class Cat:
+    def __init__(self, name, preferences):
+       self.name = name
+       self.preferences = preferences
+       self.available_space = 2000
+       self.rating = 0
+       self.collected_objects = []
+    def fill_space(self, hunt_objects):
+        for _ in range(100):
+            temp_rating, temp_objects, temp_space = 0, [], self.available_space
+            while temp_space > 0:
+                added_item = False
+                rnd.shuffle(hunt_objects)
+                for item in sorted(hunt_objects, key=lambda x: self.preferences.get(x.name, 0), reverse=True):
+                    if temp_space >= item.size[0] * item.size[1] * item.size[2] and self.preferences.get(item.name, 0) > 0:
+                        temp_objects.append(item)
+                        temp_space -= item.size[0] * item.size[1] * item.size[2]
+                        temp_rating += 0.5 + 0.5 * self.preferences.get(item.name, 0)
+                        added_item = True
+                        break
+                if not added_item:
+                    break
+            if temp_rating > self.rating:
+                self.rating = temp_rating
+                self.collected_objects = temp_objects
+        self.available_space = temp_space
+class HuntingItem:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+
 def main():
     hunt_objects = [HuntingItem('field_mouse', (5, 3, 3)), HuntingItem('house_mouse', (7, 2, 2)), HuntingItem('snail', (3, 3, 3)), HuntingItem('leaf', (3, 2, 1)), HuntingItem('rock', (2, 2, 1))]
     cats = [Cat("Ariana", {'field_mouse': 0.125, 'house_mouse': 0.125, 'snail': 0.375, 'leaf': 0.375, 'rock': 0.0}),
